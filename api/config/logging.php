acac416743ce5c08ab2ad5e5629633eab437e3e0
kona <kitami.kitano20@gmail.com>
2022-05-01 15:44:54 +0900

    :memo: change loggin.php for output log

--- api/config/logging.php
+++ api/config/logging.php
@@ -54,10 +54,15 @@ return [
             'ignore_exceptions' => false,
         ],
 
+        // 'single' => [
+        //     'driver' => 'single',
+        //     'path' => storage_path('logs/laravel.log'),
+        //     'level' => env('LOG_LEVEL', 'debug'),
+        // ],
+
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
+            'driver' => 'errorlog',
+            'level' => 'debug',
         ],
 
         'daily' => [
