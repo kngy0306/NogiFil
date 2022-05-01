0f4553caa29725ddc9cbf913b80990a809f2b02e
kona <kitami.kitano20@gmail.com>
2022-05-01 15:57:09 +0900

    :arrow_up: fix logging.php

--- api/config/logging.php
+++ api/config/logging.php
@@ -54,16 +54,16 @@ return [
             'ignore_exceptions' => false,
         ],
 
-        // 'single' => [
-        //     'driver' => 'single',
-        //     'path' => storage_path('logs/laravel.log'),
-        //     'level' => env('LOG_LEVEL', 'debug'),
-        // ],
-
         'single' => [
-            'driver' => 'errorlog',
-            'level' => 'debug',
+            'driver' => 'single',
+            'path' => storage_path('logs/laravel.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
         ],
+
+        // 'single' => [
+        //     'driver' => 'errorlog',
+        //     'level' => 'debug',
+        // ],
 
         'daily' => [
             'driver' => 'daily',
