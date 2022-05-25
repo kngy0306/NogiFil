5d795eb7b9dbac74b6af98389278cfc84444a360
kona <kitami.kitano20@gmail.com>
2022-05-25 22:11:36 +0900

    :memo: update OGP

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -11,23 +11,6 @@ root.render(
     <HelmetProvider>
       <Helmet>
         <title>NogiFil</title>
-        <meta property="og:title" content="NogiFil" />
-        <meta
-          property="og:description"
-          content="「乃木坂配信中」の動画を推しメンでフィルダリングするサービス"
-        />
-        <meta property="og:url" content="https://nogi-fil.vercel.app/" />
-        <meta
-          property="og:image"
-          content="https://nogi-fil.vercel.app/NogiFil_image.png"
-        />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:site" content="@YoKaU2" />
-        <meta
-          name="twitter:image"
-          content="https://nogi-fil.vercel.app/NogiFil_image.png"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
       </Helmet>
     </HelmetProvider>
     <App />
