26ef523d8c015a08f966a0173cde9ecaf877cb2f
kona <kitami.kitano20@gmail.com>
2022-05-25 22:03:03 +0900

    :memo: set OGP

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -3,10 +3,33 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
+    <HelmetProvider>
+      <Helmet>
+        <title>NogiFil</title>
+        <meta property="og:title" content="NogiFil" />
+        <meta
+          property="og:description"
+          content="「乃木坂配信中」の動画を推しメンでフィルダリングするサービス"
+        />
+        <meta property="og:url" content="https://nogi-fil.vercel.app/" />
+        <meta
+          property="og:image"
+          content="https://nogi-fil.vercel.app/NogiFil_image.png"
+        />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:site" content="@YoKaU2" />
+        <meta
+          name="twitter:image"
+          content="https://nogi-fil.vercel.app/NogiFil_image.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Helmet>
+    </HelmetProvider>
     <App />
   </React.StrictMode>
 )
